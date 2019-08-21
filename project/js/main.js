eb23bdc862aeacf00e0beb722955d06de119cd1e
JonathanG <gb18006@ues.edu.sv>
2019-08-21 01:56:42 -0600

    final version

--- project/js/main.js
+++ project/js/main.js
@@ -1,3 +1,7 @@
+$(document).ready(function(){
+    getPosts();
+});
+
 function showPicture(){
   // use jQuery ($ is shorthand) to find the div on the page and then change the html
   // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
@@ -28,4 +32,64 @@ function getWeather(searchQuery){
 function searchWeather(){
   var searchQuery = $(".search").val();
   getWeather(searchQuery);
+}
+/*GOOGLE LOG IN*/
+function handleSignIn(){
+  var provider = new firebase.auth.GoogleAuthProvider();
+
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    console.log(user.email);
+    
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+}
+
+function addMessage(postTitle,postBody){
+  var postData = {
+      title: postTitle,
+      body: postBody 
+  };
+
+  // Get a reference to the database service
+  var database = firebase.database().ref("posts");
+
+  var newPostRef = database.push();
+    newPostRef.set(postData, function(error){
+      if (error){
+
+      }else{
+        window.location.reload();
+      }
+    });
+}
+
+function handleMessageFormSubmit(){
+    var postTitle = $("#post-title").val();
+    var postBody = $("#post-body").val();
+    addMessage(postTitle,postBody);
+}
+
+function getPosts(){
+
+  return firebase.database().ref('posts').once('value').then(function(snapshot) {
+    var posts = snapshot.val();
+    console.log(posts);
+    for(var postKey in posts){
+        var post = posts[postKey];
+        $("#post-listing").append("<div>"+post.title+" - "+post.body+"</div>");
+    }
+  });
+
 }
